5ed1be48684c0b333f21897df1677d770a17266a
unknown <zhezhou@gatech.edu>
2021-03-22 20:08:03 -0400

    added popup.js

--- content.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => { 
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    alert(tabs[0].url)
-  });
-});
---
+++ popup.js
@@ -0,0 +1,19 @@
+// get the url
+let url = window.location.href;
+
+// perform checks
+function check() {
+    return Math.random();
+}
+// update the warning level
+
+// document.getElementById("level").innerHTML = globalUrl;
+
+// changeColor.addEventListener("click", async () => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id },
+//       function: () => {alert('123')},
+//     });
+//   });
