de2a104d88413820160b3797abe963431911d26d
unknown <zhezhou@gatech.edu>
2021-04-04 10:14:32 -0400

    can capture screenshot

--- screenshot.js
+++ screenshot.js
@@ -1,62 +1,77 @@
-var desktop_sharing = false;
-var local_stream = null;
-function toggle() {
-    if (!desktop_sharing) {
-        chrome.desktopCapture.chooseDesktopMedia(["screen", "window"], onAccessApproved);
-    } else {
-        desktop_sharing = false;
 
-        if (local_stream)
-            local_stream.stop();
-        local_stream = null;
 
-        document.querySelector('button').innerHTML = "Enable Capture";
-        console.log('Desktop sharing stopped...');
-    }
-}
 
-function onAccessApproved(desktop_id) {
-    if (!desktop_id) {
-        console.log('Desktop Capture access rejected.');
-        return;
-    }
-    desktop_sharing = true;
-    document.querySelector('button').innerHTML = "Disable Capture";
-    console.log("Desktop sharing started.. desktop_id:" + desktop_id);
 
-    navigator.webkitGetUserMedia({
-        audio: false,
-        video: {
-            mandatory: {
-                chromeMediaSource: 'desktop',
-                chromeMediaSourceId: desktop_id,
-                minWidth: 1280,
-                maxWidth: 1280,
-                minHeight: 720,
-                maxHeight: 720
-            }
-        }
-    }, gotStream, getUserMediaError);
+// var desktop_sharing = false;
+// var local_stream = null;
+// function toggle() {
+//     if (!desktop_sharing) {
+//         chrome.desktopCapture.chooseDesktopMedia(["screen", "window"], onAccessApproved);
+//     } else {
+//         desktop_sharing = false;
 
-    function gotStream(stream) {
-        local_stream = stream;
-        document.querySelector('video').src = URL.createObjectURL(stream);
-        stream.onended = function() {
-            if (desktop_sharing) {
-                toggle();
-            }
-        };
-    }
+//         if (local_stream)
+//             local_stream.stop();
+//         local_stream = null;
 
-    function getUserMediaError(e) {
-      console.log('getUserMediaError: ' + JSON.stringify(e, null, '---'));
-    }
-}
+//         document.querySelector('button').innerHTML = "Enable Capture";
+//         console.log('Desktop sharing stopped...');
+//     }
+// }
+
+// function onAccessApproved(desktop_id) {
+    // if (!desktop_id) {
+    //     console.log('Desktop Capture access rejected.');
+    //     return;
+    // }
+    // desktop_sharing = true;
+    // document.querySelector('button').innerHTML = "Disable Capture";
+    // console.log("Desktop sharing started.. desktop_id:" + desktop_id);
+
+    // navigator.webkitGetUserMedia({
+    //     audio: false,
+    //     video: {
+    //         mandatory: {
+    //             chromeMediaSource: 'desktop',
+    //             chromeMediaSourceId: desktop_id,
+    //             minWidth: 1280,
+    //             maxWidth: 1280,
+    //             minHeight: 720,
+    //             maxHeight: 720
+    //         }
+    //     }
+    // }, gotStream, getUserMediaError);
+
+    // function gotStream(stream) {
+    //     local_stream = stream;
+    //     document.querySelector('video').src = URL.createObjectURL(stream);
+    //     stream.onended = function() {
+    //         if (desktop_sharing) {
+    //             toggle();
+    //         }
+    //     };
+    // }
+
+    // function getUserMediaError(e) {
+    //   console.log('getUserMediaError: ' + JSON.stringify(e, null, '---'));
+    // }
+// }
 
 /**
  * Click handler to init the desktop capture grab
  */
+// document.querySelector('button').addEventListener('click', function(e) {
+//     toggle();
+//     console.log(chrome);
+// });
+
+
 document.querySelector('button').addEventListener('click', function(e) {
-    toggle();
-    console.log(chrome);
+    // capture screenshot
+    // default current window, default format jpeg
+    chrome.tabs.captureVisibleTab(callback = (dataUrl) => {
+        console.log(dataUrl);
+    }) 
+    
+    // encode the icon image
 });
