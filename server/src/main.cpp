c0ce2658b82c9f66c5489485042b0493a12af340
gconeice <yyang811@gatech.edu>
2021-04-17 16:04:02 -0400

    socket setup

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -4,10 +4,71 @@
 #include <algorithm>
 #include "PhishDetector.h"
 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
 using namespace std;
 
 int main()
 {
+	cout << "=== PD server starting ===" << endl;
+	// socket
+	int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenfd == -1)
+	{
+		cout << "Error: can't set up socket" << endl;
+		return -1;
+	}
+	// bind
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(44444);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    if (bind(listenfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+        std::cout << "Error: can't bind" << std::endl;
+        return -1;
+    }
+    // listen
+    if(listen(listenfd, 5) == -1) {
+        std::cout << "Error: can't listen" << std::endl;
+        return 0;
+    }
+    // accept
+    int conn;
+    char clientIP[INET_ADDRSTRLEN] = "";
+    struct sockaddr_in clientAddr;
+    socklen_t clientAddrLen = sizeof(clientAddr);
+    while (true) {
+        std::cout << "...listening" << std::endl;
+        conn = accept(listenfd, (struct sockaddr*)&clientAddr, &clientAddrLen);
+        if (conn < 0) {
+            std::cout << "Error: can't accept" << std::endl;
+            continue;
+        }
+        inet_ntop(AF_INET, &clientAddr.sin_addr, clientIP, INET_ADDRSTRLEN);
+        std::cout << "...connect " << clientIP << ":" << ntohs(clientAddr.sin_port) << std::endl;
+
+        char buf[255];
+        while (true) {
+            memset(buf, 0, sizeof(buf));
+            int len = recv(conn, buf, sizeof(buf), 0);
+            buf[len] = '\0';
+            if (strcmp(buf, "exit") == 0) {
+                std::cout << "...disconnect " << clientIP << ":" << ntohs(clientAddr.sin_port) << std::endl;
+                break;
+            }
+            std::cout << buf << std::endl;
+            send(conn, buf, len, 0);
+        }
+        
+        close(conn);
+    }
+    close(listenfd);
+    return 0;		
+	
 	string URL;
 	cin >> URL;
 	PhishDetector pd(URL);
